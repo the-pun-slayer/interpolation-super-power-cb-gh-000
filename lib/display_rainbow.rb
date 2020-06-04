4edf7a15916c1429707e1b5427d4ba5fccbea1ab
PUNEET MANDIL <puneetmandil.mec18@itbhu.ac.in>
2020-06-04 10:15:20 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -2,3 +2,5 @@
 def display_rainbow(c)
   puts "R: #{c[0]}, O: #{c[1]} ,Y: #{c[2]} ,G: #{c[3]} ,B: #{c[4]} ,I: #{c[5]} ,V: #{c[6]}"
 end
+
+display_rainbow(['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'])
